25324b38de5b93b3c84a6f6e19ef13cb56536796
Seung Yoon Lee <55730591+dltmddbs100@users.noreply.github.com>
2021-12-19 14:43:47 +0900

    Update train.py

--- code/train.py
+++ code/train.py
@@ -1,25 +1,30 @@
 import os
-from math import log10
 import glob
 import time
 from warnings import filterwarnings
 filterwarnings('ignore')
 
+import numpy as np
+import math
+from math import log10
+
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
+
 from torchvision.transforms import ToTensor, ToPILImage
 
-from loss import GeneratorLoss
+from loss import GeneratorLoss, psnr
 from dataset import TrainDataset, TestDataset
 from model import Generator, Discriminator
 
 
 # args
 args={}
-args['max_epochs']=20
-args['batch_size']=4
+args['max_epochs'] = 50
+args['batch_size'] = 4
 args['data_path'] = "/content/drive/MyDrive/Dacon/LG_Denoising/"
+args['weight_path'] = "/content/drive/MyDrive/코드정리(블로그)/논문정리/GAN/SRGAN/weights/"
 
 # dataset
 train_data=TrainDataset(args['data_path'])
@@ -44,6 +49,21 @@ if torch.cuda.is_available():
     
 optimizerG = optim.Adam(netG.parameters(),lr=1e-4)
 optimizerD = optim.Adam(netD.parameters(),lr=1e-4)
+
+netG = Generator(2)
+print('# generator parameters:', sum(param.numel() for param in netG.parameters()))
+netD = Discriminator()
+print('# discriminator parameters:', sum(param.numel() for param in netD.parameters()))
+
+generator_criterion = GeneratorLoss()
+
+if torch.cuda.is_available():
+  netG.cuda()
+  netD.cuda()
+  generator_criterion.cuda()
+    
+optimizerG = optim.Adam(netG.parameters(),lr=1e-4, weight_decay=1e-5)
+optimizerD = optim.Adam(netD.parameters(),lr=1e-4, weight_decay=1e-5)
 
 for epoch_i in range(0, args['max_epochs']):
 
@@ -107,23 +127,26 @@ for epoch_i in range(0, args['max_epochs']):
   ###########################
   netG.eval()
 
+  total_valid_psnr = 0
+  total_valid_mse = 0
   total_batch=len(test_loader)
 
   for i,batch in enumerate(test_loader):
     lr, hr = batch[0].cuda(), batch[1].cuda()
-    
-    valing_results = {'mse': 0, 'psnr': 0}
 
     with torch.no_grad():
       sr=netG(lr)
 
     batch_mse = ((sr - hr) ** 2).data.mean()
-    valing_results['mse'] += batch_mse * args['batch_size']
-    valing_results['psnr'] += 10 * log10((hr.max()**2) / (valing_results['mse'] / args['batch_size']))
+    total_valid_mse += batch_mse * args['batch_size']
+    total_valid_psnr += psnr(hr,sr)
 
-    print(f"\rTest Batch {i+1}/{total_batch} , mse : {valing_results['mse']/(i+1):.6f}, psnr : {valing_results['psnr']/(i+1):.6f}", end='')
+    print(f"\rTest Batch {i+1}/{total_batch} , mse : {total_valid_mse/(i+1):.6f}, psnr : {total_valid_psnr/(i+1):.6f}", end='')
 
   # show the result among test datasets samples
   lr = test_data.__getitem__(13)[0].cuda()
   sr=netG(lr.unsqueeze(0))
   display(ToPILImage()(torch.clamp(sr[0].data.cpu(),0,1)))
+
+  if (epoch_i+1)%10==0:
+    torch.save(netG.state_dict(), args['weight_path']+f'netG_epoch_{epoch_i+1}_upscale_2.pth')
